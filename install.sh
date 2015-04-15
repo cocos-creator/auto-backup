2e8aa415a3455a2598606d10714894308ce3369e
Yazhong Liu <yorkiefixer@gmail.com>
2015-04-15 16:29:24 +0800

    update

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 
-uname -a
-
 curl -O http://downloads.mongodb.org/linux/mongodb-linux-x86_64-3.0.2.tgz
 tar -zxvf mongodb-linux-x86_64-3.0.2.tgz
 mkdir -p mongodb
 cp -R -n mongodb-linux-x86_64-3.0.2/ mongodb
 
-ls ./mongodb/
+mv mongodb-linux-x86_64-3/bin/mongodump ./mongodump
+ls ./
+
+sh backup.sh
